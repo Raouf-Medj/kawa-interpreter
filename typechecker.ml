8b5a56a72b8c5971e66e564ccd2d9b210822809f
Raouf-Medj <Raouf-Medj@github.com>
2025-01-10 23:31:29 +0100

    validated typechecker and added rec test

--- typechecker.ml
+++ typechecker.ml
@@ -9,8 +9,8 @@ let type_error ty_actual ty_expected =
 module Env = Map.Make(String)
 type tenv = typ Env.t
 
-  let add_env l tenv =
-    List.fold_left (fun env (x, t) -> Env.add x t env) tenv l
+let add_env l tenv =
+  List.fold_left (fun env (x, t) -> Env.add x t env) tenv l
 let typecheck_prog p =
   let tenv = add_env p.globals Env.empty in
 
@@ -24,8 +24,20 @@ let typecheck_prog p =
 
   and check e typ tenv =
     let typ_e = type_expr e tenv in
-    if typ_e <> typ then type_error typ_e typ
+    match typ_e, typ with
+    | TClass cls1, TClass cls2 -> 
+      if not (class_incluse p.classes cls1 cls2) 
+        then error ("Class " ^ cls1 ^ " is not a subclass of " ^ cls2)
+    | _ -> if typ_e <> typ then type_error typ_e typ
 
+  and class_incluse classes cname1 cname2 =
+    if cname1 = cname2 then true
+    else
+      let cls = find_class classes cname1 in
+      match cls.parent with
+      | Some parent -> class_incluse classes parent cname2
+      | None -> false
+  
   and type_expr e tenv = match e with
     | Int _  -> TInt
     | Bool _ -> TBool
@@ -38,24 +50,29 @@ let typecheck_prog p =
         | TBool -> TBool
         | ty -> type_error ty TBool)
     | Binop (op, e1, e2) ->
-        (match op, type_expr e1 tenv, type_expr e2 tenv with
-        | (Add | Sub | Mul | Div | Rem), TInt, TInt -> TInt
-        | (Lt | Le | Gt | Ge), TInt, TInt -> TBool
-        | (Eq | Neq), t1, t2 when t1 = t2 -> TBool
-        | (And | Or), TBool, TBool -> TBool
-        | _ -> error "Invalid binary operation or operand types")
+      (match op with
+      | Add | Sub | Mul | Div | Rem ->
+          check e1 TInt tenv; check e2 TInt tenv; TInt
+      | Lt | Le | Gt | Ge ->
+          check e1 TInt tenv; check e2 TInt tenv; TBool
+      | Eq | Neq ->
+          let t1 = type_expr e1 tenv in
+          let t2 = type_expr e2 tenv in
+          if t1 <> t2 then type_error t2 t1;
+          TBool
+      | And | Or ->
+          check e1 TBool tenv; check e2 TBool tenv; TBool)
     | Get (Var x) -> (try Env.find x tenv with Not_found -> error ("Variable not found: " ^ x))
     | Get (Field (e, field)) ->
         (match type_expr e tenv with
         | TClass cname ->
             let cls = find_class p.classes cname in
-            (try List.assoc field cls.attributes
-            with Not_found -> error ("Field not found: " ^ field))
+            find_field cls field
         | ty -> type_error ty (TClass "object"))
-    | This -> TClass "current" 
-    | New cname ->
-        let _ = find_class p.classes cname in
-        TClass cname
+    | This -> 
+      (try Env.find "this" tenv with Not_found -> error ("Unbound 'this' in the current context"))
+    | New cname -> 
+      let _ = find_class p.classes cname in TClass cname
     | NewCstr (cname, args) ->
         let cls = find_class p.classes cname in
         let cstr_params =
@@ -65,12 +82,7 @@ let typecheck_prog p =
         in
         if List.length cstr_params <> List.length args then
           error "Constructor argument count mismatch";
-        List.iter2
-          (fun param_type arg ->
-            let arg_type = type_expr arg tenv in
-            if param_type <> arg_type then
-              type_error arg_type param_type)
-          cstr_params args;
+        List.iter2 (fun param_type arg -> check arg param_type tenv) cstr_params args;
         TClass cname
     | MethCall (obj, mname, args) ->
         (match type_expr obj tenv with
@@ -79,34 +91,23 @@ let typecheck_prog p =
             let method_ = find_method cls mname in
             if List.length method_.params <> List.length args then
               error "Method argument count mismatch";
-            List.iter2
-              (fun (_, param_type) arg ->
-                let arg_type = type_expr arg tenv in
-                if param_type <> arg_type then
-                  type_error arg_type param_type)
-              method_.params args;
+            List.iter2 (fun (_, param_type) arg -> check arg param_type tenv) method_.params args;
             method_.return
         | ty -> type_error ty (TClass "object"))
-    (* | EArrayCreate (array_type, size_expr) ->
-        check size_expr TInt tenv;
-        TArray array_type
-        | EArrayGet (arr_expr, index_expr) | Get(ArrayAccess(arr_expr, index_expr))->
-          (match type_expr arr_expr tenv with
-        | TArray elem_type ->
-            check index_expr TInt tenv;
-            elem_type
-        | ty -> type_error ty (TArray TInt))
-    | EArraySet (arr_expr, index_expr, value_expr) ->
-        (match type_expr arr_expr tenv with
-        | TArray elem_type ->
-            check index_expr TInt tenv;
-            check value_expr elem_type tenv;
-            TVoid
-        | ty -> type_error ty (TArray TInt)) *)
+  
+  and find_field cls field =
+    try List.assoc field cls.attributes
+    with Not_found ->
+      match cls.parent with
+      | Some parent_name ->
+          let parent_cls = find_class p.classes parent_name in
+          find_field parent_cls field
+      | None -> error ("Field not found: " ^ field)
 
   and check_instr i ret tenv = match i with
-    | Print e -> (try check e TInt tenv with 
-      exn ->
+    | Print e -> (
+      try check e TInt tenv 
+      with exn ->
         try check e TBool tenv
         with exn -> check e TVoid tenv)
     | Set (Var x, e) ->
@@ -116,19 +117,9 @@ let typecheck_prog p =
         (match type_expr obj tenv with
         | TClass cname ->
             let cls = find_class p.classes cname in
-            let tfield =
-              try List.assoc field cls.attributes
-              with Not_found -> error ("Field not found: " ^ field)
-            in
+            let tfield = find_field cls field in
             check e tfield tenv
         | ty -> type_error ty (TClass "object"))
-    (* | Set (ArrayAccess (arr_expr, index_expr), value_expr) |ArraySet(ArrayAccess (arr_expr, index_expr), value_expr)->
-        (match type_expr arr_expr tenv with
-        | TArray elem_type ->
-            check index_expr TInt tenv;
-            check value_expr elem_type tenv
-        | ty -> type_error ty (TArray TInt))
-    |ArraySet(e,_)-> type_error (type_expr (Get e) tenv) (TArray TInt) *)
     | If (cond, then_seq, else_seq) ->
         check cond TBool tenv;
         check_seq then_seq ret tenv;
@@ -137,7 +128,7 @@ let typecheck_prog p =
         check cond TBool tenv;
         check_seq body ret tenv
     | Return e -> check e ret tenv
-    | Expr e -> ignore (type_expr e tenv)
+    | Expr e -> check e TVoid tenv
 
   and check_seq s ret tenv =
     List.iter (fun i -> check_instr i ret tenv) s
@@ -148,7 +139,8 @@ let typecheck_prog p =
 
   and check_class cls tenv =
     let class_env = add_env cls.attributes tenv in
-    List.iter (fun method_ -> check_method cls method_ class_env) cls.methods
+    let class_env_this = Env.add "this" (TClass(cls.class_name)) class_env in
+    List.iter (fun method_ -> check_method cls method_ class_env_this) cls.methods
 
   in
   List.iter (fun cls -> check_class cls tenv) p.classes;
