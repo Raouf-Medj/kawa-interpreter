67c75bfc971799b2ad61b6855a8e6907081c975b
Zieir <zineddinekermadj123@gmail.com>
2025-01-14 14:55:30 +0100

    added types

--- gast.ml
+++ gast.ml
@@ -15,42 +15,43 @@ module Dot = Graph.Graphviz.Dot(struct
   let default_edge_attributes _ = []
   let get_subgraph _ = None
   let vertex_attributes v = [`Label v]
-  let vertex_name v = String.escaped v
+  let vertex_name v = "\"" ^ String.escaped v ^ "\""  (* Échappe les noms *)
   let default_vertex_attributes _ = []
   let graph_attributes _ = []
 end)
+
 
 let export_to_image prog output_file =
   (* Création du graphe *)
   let g = G.create () in
 
   (* Ajout de la racine : le programme lancé *)
-  let root_node = "Program" in
+  let root_node = "Program (Root)" in
   G.add_vertex g root_node;
 
   (* Ajout des variables globales comme fils de la racine *)
-  List.iter (fun (var_name, _) ->
-    let var_node = "Global_" ^ var_name in
+  List.iter (fun (var_name, var_type) ->
+    let var_node = Printf.sprintf "Global: %s : %s (Variable)" var_name (typ_to_string var_type) in
     G.add_vertex g var_node;
     G.add_edge g root_node var_node
   ) prog.globals;
 
   (* Ajout des classes et de leur structure *)
   List.iter (fun cls ->
-    let class_node = "Class_" ^ cls.class_name in
+    let class_node = Printf.sprintf "Class: %s (Class)" cls.class_name in
     G.add_vertex g class_node;
     G.add_edge g root_node class_node;
 
     (* Ajout des attributs comme fils de la classe *)
-    List.iter (fun (attr_name, _) ->
-      let attr_node = cls.class_name ^ "_Attr_" ^ attr_name in
+    List.iter (fun (attr_name, attr_type) ->
+      let attr_node = Printf.sprintf "Attr: %s : %s (Attribute)" attr_name (typ_to_string attr_type) in
       G.add_vertex g attr_node;
       G.add_edge g class_node attr_node
     ) cls.attributes;
 
     (* Ajout des méthodes comme fils de la classe *)
     List.iter (fun meth ->
-      let method_node = cls.class_name ^ "_Meth_" ^ meth.method_name in
+      let method_node = Printf.sprintf "Meth: %s (Method)" meth.method_name in
       G.add_vertex g method_node;
       G.add_edge g class_node method_node
     ) cls.methods
@@ -69,3 +70,4 @@ let export_to_image prog output_file =
     Printf.eprintf "Erreur lors de la génération de l'image\n"
   else
     Printf.printf "Image générée avec succès : %s\n" output_file
+;;
